0f8b00ccb58652d8f1b98857b7ecbc88baffd0b2
Sam Ritchie <sritchie09@gmail.com>
2012-02-27 13:59:20 -0800

    Stop double-serializing class.

--- src/jvm/backtype/hadoop/EnumDeserializer.java
+++ src/jvm/backtype/hadoop/EnumDeserializer.java
@@ -2,31 +2,33 @@ package backtype.hadoop;
 
 import org.apache.hadoop.io.WritableUtils;
 import org.apache.hadoop.io.serializer.Deserializer;
+import org.apache.thrift.TEnum;
 
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Serializable;
 import java.lang.reflect.Method;
 
 /** User: sritchie Date: 2/8/12 Time: 9:50 PM */
-public class EnumDeserializer<T extends Serializable> implements Deserializer<T> {
+public class EnumDeserializer implements Deserializer<TEnum> {
     private DataInputStream inStream;
+    Class<TEnum> enumClass;
 
+    public EnumDeserializer(Class<TEnum> c) {
+        enumClass = c;
+    }
     public void open(InputStream in) throws IOException {
         inStream = new DataInputStream(in);
     }
 
-    public T deserialize(T obj) throws IOException {
-        String klassName = WritableUtils.readString(inStream);
+    public TEnum deserialize(TEnum obj) throws IOException {
         int val = WritableUtils.readVInt(inStream);
 
         Class[] args = new Class[] {Integer.TYPE};
         try {
-            Class klass = Class.forName(klassName);
-            Method method = klass.getDeclaredMethod("findByValue", args);
+            Method method = enumClass.getDeclaredMethod("findByValue", args);
             Object[] argVec = new Object[]{val};
-            return (T) method.invoke(null, argVec);
+            return (TEnum) method.invoke(null, argVec);
         } catch (Exception e) {
             throw new IOException(e.toString());
         }
--- src/jvm/backtype/hadoop/EnumSerializer.java
+++ src/jvm/backtype/hadoop/EnumSerializer.java
@@ -7,17 +7,15 @@ import org.apache.thrift.TEnum;
 import java.io.*;
 
 /** User: sritchie Date: 2/8/12 Time: 9:49 PM */
-public class EnumSerializer<T extends Serializable> implements Serializer<T> {
+public class EnumSerializer implements Serializer<TEnum> {
     private DataOutputStream outStream;
 
     public void open(OutputStream out) throws IOException {
         outStream = new DataOutputStream(out);
     }
 
-    public void serialize(T obj) throws IOException {
-        TEnum enumObject = (TEnum) obj;
-        WritableUtils.writeString(outStream, obj.getClass().getName());
-        WritableUtils.writeVInt(outStream, enumObject.getValue());
+    public void serialize(TEnum obj) throws IOException {
+        WritableUtils.writeVInt(outStream, obj.getValue());
     }
 
     public void close() throws IOException {
