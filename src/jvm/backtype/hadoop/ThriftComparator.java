24ac781b7770325a35fc0e416e25ee912ae7d98c
Sam Ritchie <sritchie@tw-mbp13-sritchie.local>
2012-04-05 14:17:33 -0700

    Removed stream comparison.

--- src/jvm/backtype/hadoop/ThriftComparator.java
+++ src/jvm/backtype/hadoop/ThriftComparator.java
@@ -1,43 +1,15 @@
 package backtype.hadoop;
 
-import cascading.tuple.StreamComparator;
-import cascading.tuple.hadoop.BufferedInputStream;
-import org.apache.hadoop.io.WritableComparator;
-import org.apache.hadoop.io.WritableUtils;
 import org.apache.thrift.TBase;
 import org.apache.thrift.TEnum;
 
-import java.io.DataInputStream;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.Comparator;
 
 
-public class ThriftComparator implements StreamComparator<BufferedInputStream>, Comparator, Serializable {
+public class ThriftComparator implements Comparator, Serializable {
+
     public ThriftComparator(Class type) {
-    }
-
-    public int compare(BufferedInputStream lhsStream, BufferedInputStream rhsStream) {
-        try {
-            //TODO: how can i get rid of creating new stream objects everytime?
-            DataInputStream dleft = new DataInputStream(lhsStream);
-            DataInputStream dRight = new DataInputStream(rhsStream);
-
-            int lhsLen = WritableUtils.readVInt(dleft);
-            byte[] lhs = lhsStream.getBuffer();
-            int lhsPos = lhsStream.getPosition();
-
-            int rhsLen = WritableUtils.readVInt(dRight);
-            byte[] rhs = rhsStream.getBuffer();
-            int rhsPos = rhsStream.getPosition();
-
-            lhsStream.skip(lhsLen);
-            rhsStream.skip(rhsLen);
-
-            return WritableComparator.compareBytes( lhs, lhsPos, lhsLen, rhs, rhsPos, rhsLen );
-        } catch(IOException ioe) {
-            throw new RuntimeException(ioe);
-        }
     }
 
     public int compare(Object o1, Object o2) {
