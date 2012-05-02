f56b757e6b319b4e09876039dc817347d9090042
Sam Ritchie <sritchie09@gmail.com>
2012-05-02 15:52:38 -0700

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject backtype/cascading-thrift "0.2.5-SNAPSHOT"
+(defproject backtype/cascading-thrift "0.2.5"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
