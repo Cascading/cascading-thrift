d0f0c03aa32c0e8bdf00886988c342960276b23e
Sam Ritchie <sritchie09@gmail.com>
2012-05-02 15:54:28 -0700

    Bump to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject backtype/cascading-thrift "0.2.5"
+(defproject backtype/cascading-thrift "0.2.6-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
